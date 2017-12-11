ac735ff12dacb03b8bc34cff13dfa68dba014497
ckwongloy <ckwongloy@gmail.com>
2017-12-11 13:27:35 +0800

    add root init dbseed

---
+++ app/dat/dbseed/InitRootUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lif\Dat\Dbseed;
+
+use Lif\Core\Storage\Dit;
+
+class InitRootUser extends Dit
+{
+    public function commit()
+    {
+        if (schema()->hasTable('user')) {
+            db()
+            ->table('user')
+            ->insert([
+                'name'    => 'root',
+                'account' => 'root',
+                'email'   => 'root@localhost',
+                'passwd'  => '2y$10$8s.A665uEoKew7Zc2zNYAuI3Qk3hiQPV7/Ix0XjrtXTSHFp0xHd3e',
+                'role'    => 'admin',
+                'status'  => 1,
+            ]);
+        }
+    }
+
+    public function revert()
+    {
+        if (schema()->hasTable('user')) {
+            db()
+            ->table('user')
+            ->whereAccount('root')
+            ->delete();
+        }
+    }
+}
