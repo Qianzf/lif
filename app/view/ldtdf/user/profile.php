792fdd0a90a06ca3b3e58832b7bfc5ce1162a01d
ckwongloy <ckwongloy@gmail.com>
2017-12-12 15:39:13 +0800

    fix passwd update nothing output in ldtdf

--- app/view/ldtdf/user/profile.php
+++ app/view/ldtdf/user/profile.php
@@ -1,4 +1,4 @@
-<?= $this->layout('main') ?>
+'<?= $this->layout('main') ?>
 <?= $this->section('back2list', [
     'model' => collect([
         'id' => true,
@@ -22,13 +22,13 @@
 
     <label>
         <span class="label-title"><?= L('OLD_PASSWORD') ?></span>
-        <input type="password" name="passwordOld"
+        <input type="password" name="pswdold"
         placeholder="<?= L('PROVIDE_OLD_PASSWD') ?>">
     </label>
 
     <label>
         <span class="label-title"><?= L('NEW_PASSWORD') ?></span>
-        <input type="password" name="passwordNew"
+        <input type="password" name="pswdnew"
         placeholder="<?= L('PROVIDE_NEW_PASSWD') ?>">
     </label>
 
